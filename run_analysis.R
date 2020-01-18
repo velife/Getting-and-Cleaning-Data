2e58ac86dfca911bc67c24fd3caecc9077c146b7
velife <ve.life@yandex.ru>
2020-01-18 21:15:50 +1000

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+library(data.table)
+library(dplyr)
+
+path <- getwd()
+
+#download data
+url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+f <- 'Dataset.zip'
+if(!file.exists(f)) {download.file(url,f)}
+d <- 'UCI HAR Dataset'
+if(!file.exists(d)) {unzip(f)}
+
+#read subjects
+dtSubjTrain <- data.table(read.table(file.path(path, d, 'train', 'subject_train.txt')))
+dtSubjTest <- data.table(read.table(file.path(path, d, 'test', 'subject_test.txt')))
+dtSubj <- rbind(dtSubjTrain, dtSubjTest)
+names(dtSubj) <- c('Subject')
+remove(dtSubjTrain,dtSubjTest)
+
+#read activities
+dtActTrain <- data.table(read.table(file.path(path, d, 'train','Y_train.txt')))
+dtActTest <- data.table(read.table(file.path(path,d,'test','Y_test.txt')))
+dtAct <- rbind(dtActTrain,dtActTest)
+names(dtAct) <- c('Activity')
+remove(dtActTrain,dtActTest)
+
+#combine subject and activity
+dtSubj <- cbind(dtSubj,dtAct)
+remove(dtAct)
+
+#read feature data
+dtTrain <- data.table(read.table(file.path(path,d,'train','X_train.txt')))
+dtTest <- data.table(read.table(file.path(path,d,'test','X_test.txt')))
+dt <- rbind(dtTrain,dtTest)
+remove(dtTrain,dtTest)
+
+#merge into one table subject/activity/feature
+dt <- cbind(dtSubj,dt)
+setkey(dt,Subject,Activity)
+remove(dtSubj)
+
+#read feature names
+dtFeats <- data.table(read.table(file.path(path,d,'features.txt'))) 
+names(dtFeats) <- c('ftNum','ftName')
+dtFeats <- dtFeats[grepl("mean\\(\\)|std\\(\\)",ftName)]
+dtFeats$ftCode <- paste('V', dtFeats$ftNum, sep = "")
+dt <- dt[,c(key(dt), dtFeats$ftCode),with=F]
+
+
